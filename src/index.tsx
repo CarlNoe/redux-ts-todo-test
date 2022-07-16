c58cfca9e9a5f2f4f8276532f30baf34a1893b6a
Noé Carl <noe.carldev@gmail.com>
2022-07-16 23:34:57 +0200

    Test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App/App";
 
 export default function index() {
   return <div>index</div>;
 }
+
+ReactDOM.render(<App />, document.getElementById("root"));
